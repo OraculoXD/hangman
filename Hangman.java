2b0512aedd52692ddf7f9e2351703b770f742b3f
gabriel <gabriel.almeida98@edu.pucrs.br>
2022-10-05 10:29:01 -0300

    Continuação

--- Hangman.java
+++ Hangman.java
@@ -1,49 +1,61 @@
 import java.util.Scanner;
 
-public class Hangman{
-    private static char nextGuess(Scanner sc){
+public class Hangman {
+    private static char nextGuess(Scanner in) {
         char guess;
         String line;
-        guess = sc.next().charAt(0);
-        
-        line = sc.nextLine();
+        line = in.nextLine();
+        guess = line.charAt(0);
         return guess;
     }
     
-    private static void printHeader(){
+    private static void printHeader() {
         System.out.println("Jogo da Forca");
         System.out.println("=============");
-        System.out.println();
+        System.out.println();        
     }
     
-    public static void main(String[] args){
-        Scanner sc;
-        char guess;
-        String secret;
-        String word;
-        String wrongs;
+    private static String returnWord(String secret) {
+        String word = "";
+        int i = 0;
+        while (i < secret.length() ){
+            word = word + "_";
+            i++;
+        }
+        return word;
+    }
+    
+    public static String returnCorret(String secret, String word, char guess) {
+        return "";
+    }
+    
+    public static void main(String[] args) {
+        Scanner in;
+        char guess;             // a letra que foi digitada
+        String secret;          // a palavra que deve ser adivinhada
+        String word;            // as letras que foram adivinhadas
+        String wrongs;          // letras que foram tentadas e erradas
         
         printHeader();
-                
-        secret = "hangman";
-        word   = "_______";
-        wrongs  = "";
         
-        sc = new Scanner(System.in);
+        secret = "java";
+        word   = returnWord(secret);
         
+        wrongs = "";
+        
+        in = new Scanner(System.in);
+       
         System.out.printf("Palavra:   %s\n", word);
-        System.out.printf("Tentativa: ");
-        guess = nextGuess(sc);
-        
-        System.out.println(guess);
+        System.out.print( "Tentativa: ");
+        guess = nextGuess(in);
         
         if (secret.contains("" + guess)) {
-            
+            word = returnCorret(secret, word, guess);
         } else {
             wrongs = wrongs + guess + " ";
         }
         System.out.printf("Erradas:   %s\n", wrongs);
         
-        sc.close();
+        in.close();
     }
 }
